b7001abb49719e03c08cb6d110669dbd659229ac
thai nguyen <thai nguyen>
2016-08-08 19:17:06 +0700

    08082016

--- modules/custom/thp/js/thp.js
+++ modules/custom/thp/js/thp.js
@@ -14,28 +14,66 @@
     attach: function (context, settings) {
 
       // Left Menu click
-      $('#menu-bnt').click(function(){
-        if ($('#block-block-2.active').length < 1) {
-          $('#block-block-2').addClass('active').show("slide", {direction: "left" }, 300);
-          $(this).addClass('active');
-        }else {
-          $('#block-block-2').removeClass('active').hide("slide", {direction: "left" }, 300);
-          $(this).removeClass('active');
-        }
-      });
+      if ($('#menu-bnt').length > 0) {
+        $('#menu-bnt').click(function(){
+          if ($('#block-block-2.active').length < 1) {
+            $('#block-block-2').addClass('active').show("slide", {direction: "left" }, 300);
+            $(this).addClass('active');
+          }else {
+            $('#block-block-2').removeClass('active').hide("slide", {direction: "left" }, 300);
+            $(this).removeClass('active');
+          }
+        });
+      }
       
       // Aseptic Menu click
-      $('#block-views-aseptic-block-1 .views-row').click(function(){
-        if (!$(this).hasClass('active')) {
-          $('#block-views-aseptic-block-1 .views-row').removeClass('active');
-          $(this).addClass('active');
+      if ($('#block-views-aseptic-block-1 .views-row').length > 0) {
+        $('#block-views-aseptic-block-1 .views-row').click(function(){
+          if (!$(this).hasClass('active')) {
+            $('#block-views-aseptic-block-1 .views-row').removeClass('active');
+            $(this).addClass('active');
 
-          var ind = $(this).index();
-          $('#block-views-aseptic-block .views-row').css('left','100vw');
-          $($('#block-views-aseptic-block .views-row').get(ind)).animate({'left':'0'}, 500, 'swing');
-        }
+            var ind = $(this).index();
+            $('#block-views-aseptic-block .views-row').css('left','100vw');
+            $($('#block-views-aseptic-block .views-row').get(ind)).animate({'left':'0'}, 500, 'swing');
+          }
+
+        });
+        $('#block-views-aseptic-block .views-row-first').css('left', '0');
+      }
+      
+      
+      // Reorder parts title
+      if ($('#block-views-aseptic-block .views-row').length > 0) {
+        $('#block-views-aseptic-block .views-row').each(function(index, element) {
+          if ($('.views-field-field-parts .field-collection-item-field-parts', element).length > 0) {
+            $('.views-field-field-parts .field-collection-item-field-parts', element).each(function(ind, elem){
+              $('.views-field-nothing .field-content', element).append($('.field-name-field-title', elem));
+            });
+          }
+        });
+      }
+      
+      // Summary process
+      if ($('#block-views-aseptic-block .views-row.views-row-first .views-field-nothing').length > 0) {
+        var width = $('#block-views-aseptic-block .views-row.views-row-first .views-field-nothing').width();
+        var height = $('#block-views-aseptic-block .views-row.views-row-first .views-field-nothing').height();
+        var x = '';
+        var path = 'M';
+        var circles = '';
         
-      });
+        $('#block-views-aseptic-block .views-row.views-row-first .views-field-nothing .field-name-field-title').each(function(index, element) {
+          x = getCenterDiv(element);
+          circles += '<circle cx="' + x + '" cy="50" r="2" />';
+          path += (index === 0)? x + ' 50 ':'L' + x + ' 50 ' ;
+        });
+        
+        var svg = pathAnimate(path, width, height);
+        
+        $('#block-views-aseptic-block .views-row.views-row-first .views-field-nothing').prepend(svg);
+        $('#block-views-aseptic-block .views-row.views-row-first .views-field-nothing .dash-line').prepend(circles);
+      }
+      
     }
   };
   
@@ -55,14 +93,33 @@
         "stroke:#000000"
       ];
    */
-  function pathAnimate(path, style, width, height) {
+  function pathAnimate(path, width, height) {
+    var style = [
+      "fill:none",
+      "stroke:black",
+      "stroke-width:1",
+      "stroke-linejoin:round",
+      "stroke-linecap:round",
+      "stroke-dasharray:1000",
+      "animation: dash 3s 1 linear forwards",
+      "stroke:#FFFFFF"
+    ];
+    
     var svg = '';
     
-    svg = "<svg width='" + width + "' height='" + height + "' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xml:space='preserve'>";
+    svg = "<svg class='dash-line' width='" + width + "' height='" + height + "' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xml:space='preserve'>";
     svg += "<path d='" + path + "' style='" + style.join(";") + "' />";
     svg += "</svg>";
 
     return svg;
-   }
+  }
+  
+  function getCenterDiv(container) {
+    var offset = $(container).offset();
+    var width = $(container).width();
+    var x = offset.left + width / 2;
+    
+    return x;
+  }
 
 })(jQuery, Drupal, this, this.document);
