f03f838f0126eeb94ce8b9fcbddeeaf379864ba2
thai nguyen <thai nguyen>
2016-08-19 01:49:41 +0700

    1808 night

--- modules/custom/thp/js/treejs/js/build3dBackground.js
+++ modules/custom/thp/js/treejs/js/build3dBackground.js
@@ -163,6 +163,13 @@ function build360Img(container, materialPaths) {
       targetList.push(point);
     });
     
+    // Add word
+    var spritey = makeTextSprite(
+                "World!",
+                {fontsize: 32, fontface: "Georgia", borderColor: {r:0, g:0, b:255, a:1.0}}
+              );
+      spritey.position.set(190,50,450);
+      scene.add(spritey);
 	}
 
 	//-----------------//
@@ -207,8 +214,6 @@ function build360Img(container, materialPaths) {
 
 		onPointerDownLon = lon;
 		onPointerDownLat = lat;
-    
-    //console.log("Click.");
 	
     // update the mouse variable
     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
@@ -217,40 +222,49 @@ function build360Img(container, materialPaths) {
     // find intersections
 
     // create a Ray with origin at the mouse position
-    //   and direction into the scene (camera direction)
+    // and direction into the scene (camera direction)
     var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
     projector.unprojectVector( vector, camera );
     var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
 
     // create an array containing all objects in the scene with which the ray intersects
     var intersects = ray.intersectObjects(targetList);
-//    console.log(targetList);
-//    console.log(intersects);
+
     // if there is one (or more) intersections
     if ( intersects.length > 0 ) {
-      
-//      console.log("Hit @ " + toString( intersects[0].point ) );
-//      console.log(intersects[0]);
-      console.log(jQuery('#' + container).parent()); 
-      console.log(jQuery('.field-name-field-title', jQuery('#' + container).parent()).get(intersects[0].object.name));
       jQuery(jQuery('.field-name-field-title', jQuery('#' + container).parent()).get(intersects[0].object.name)).trigger('click');
     }
 	}
 
 	//-----------------------//
-	function onDocumentMouseMove( event ) {
-		if ( isUserInteracting === true ) {
-      
-			lon = ( onPointerDownPointerX - event.clientX ) * 0.1 + onPointerDownLon;
+	function onDocumentMouseMove(event) {
+		if (isUserInteracting === true) {
+			lon = (onPointerDownPointerX - event.clientX) * 0.1 + onPointerDownLon;
 		}
+    // update the mouse variable
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    
+    var vector = new THREE.Vector3(mouse.x, mouse.y, 1);
+    projector.unprojectVector(vector, camera);
+    var ray = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
+
+    // create an array containing all objects in the scene with which the ray intersects
+    var intersects = ray.intersectObjects(targetList);
+
+    // if there is one (or more) intersections
+    if (intersects.length > 0) {
+      
+    }else {
+      //console.log('b');
+      
+    }
 	}
 
 	//-------------------------------//
 
 	function onDocumentMouseUp( event ) {
-
 		isUserInteracting = false;
-
 	}
 
 	//----------------------------//
@@ -260,7 +274,6 @@ function build360Img(container, materialPaths) {
 	// WebKit
 
 	if ( event.wheelDeltaY ) {
-
 		camera.fov -= event.wheelDeltaY * 0.05;
 
 		// Opera / Explorer 9
@@ -349,4 +362,78 @@ function build360Img(container, materialPaths) {
 		renderer.render( scene, camera );
 
 	}
+  
+  function makeTextSprite( message, parameters ) {
+    if ( parameters === undefined ) parameters = {};
+
+    var fontface = parameters.hasOwnProperty("fontface") ? 
+      parameters["fontface"] : "Arial";
+
+    var fontsize = parameters.hasOwnProperty("fontsize") ? 
+      parameters["fontsize"] : 18;
+
+    var borderThickness = parameters.hasOwnProperty("borderThickness") ? 
+      parameters["borderThickness"] : 4;
+
+    var borderColor = parameters.hasOwnProperty("borderColor") ?
+      parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
+
+    var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
+      parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
+      
+    //var spriteAlignment = THREE.SpriteAlignment.topLeft;
+    
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    context.font = "Bold " + fontsize + "px " + fontface;
+
+    // get size data (height depends only on font size)
+    var metrics = context.measureText( message );
+    var textWidth = metrics.width;
+
+    // background color
+    context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + ","
+                    + backgroundColor.b + "," + backgroundColor.a + ")";
+    // border color
+    context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + ","
+                    + borderColor.b + "," + borderColor.a + ")";
+
+    context.lineWidth = borderThickness;
+    roundRect(context, borderThickness/2, borderThickness/2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+    // 1.4 is extra height factor for text below baseline: g,j,p,q.
+
+    // text color
+    context.fillStyle = "rgba(0, 0, 0, 1.0)";
+
+    context.fillText( message, borderThickness, fontsize + borderThickness);
+
+    // canvas contents will be used for a texture
+    var texture = new THREE.Texture(canvas) 
+    texture.needsUpdate = true;
+
+    var spriteMaterial = new THREE.SpriteMaterial( 
+      { map: texture } );
+    var sprite = new THREE.Sprite( spriteMaterial );
+    sprite.scale.set(100,50,1.0);
+    return sprite;	
+  }
+
+  // function for drawing rounded rectangles
+  function roundRect(ctx, x, y, w, h, r) {
+    ctx.beginPath();
+    ctx.moveTo(x+r, y);
+    ctx.lineTo(x+w-r, y);
+    ctx.quadraticCurveTo(x+w, y, x+w, y+r);
+    ctx.lineTo(x+w, y+h-r);
+    ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
+    ctx.lineTo(x+r, y+h);
+    ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+    ctx.lineTo(x, y+r);
+    ctx.quadraticCurveTo(x, y, x+r, y);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();   
+  }
+
+
 }
